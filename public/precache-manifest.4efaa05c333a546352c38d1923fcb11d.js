5496e7ab8e1180f95a90e1b629a32c30ba0e602a
unknown <jhonjaider100015@gmail.com>
2020-09-22 01:03:42 -0500

    Se sube el proyecto

---
+++ public/precache-manifest.4efaa05c333a546352c38d1923fcb11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85577cd5502f5200bbe4094816ca4ee2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e168248c8e204403828",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2518b8190df7c3391ba9",
+    "url": "./static/js/2.189ca9eb.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "./static/js/2.189ca9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e168248c8e204403828",
+    "url": "./static/js/main.eddcc12a.chunk.js"
+  },
+  {
+    "revision": "a7809c03c3c129a7d2a2",
+    "url": "./static/js/runtime-main.958c92b4.js"
+  }
+]);
---
+++ public/precache-manifest.d2f8a8bca2a2dc5d8fefa03b3d324533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1b4d2d3ea9bc69c2abd95d0bd25944",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ee09f2886a9eb4a6b78",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2518b8190df7c3391ba9",
+    "url": "./static/js/2.189ca9eb.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "./static/js/2.189ca9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee09f2886a9eb4a6b78",
+    "url": "./static/js/main.b13b0730.chunk.js"
+  },
+  {
+    "revision": "a7809c03c3c129a7d2a2",
+    "url": "./static/js/runtime-main.958c92b4.js"
+  }
+]);
