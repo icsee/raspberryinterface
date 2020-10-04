ec107fcf038f3c90e78ca3f1274be738e6c40a6c
jhonjaider1000 <jhonjaider100015@gmail.com>
2020-10-04 01:29:30 -0500

    Se actualiza el puerto y se incluyen los requerimientos nuevos.

---
+++ public/precache-manifest.76a3bee503e18549d54558229b9cbd72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe7f6d59d2e1933469309b57bc44e44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0fbfcc1ac96d78ea51e",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "2dd12f2c53e98cd8e828",
+    "url": "./static/js/2.1c39086a.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "./static/js/2.1c39086a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fbfcc1ac96d78ea51e",
+    "url": "./static/js/main.c5a68119.chunk.js"
+  },
+  {
+    "revision": "a7809c03c3c129a7d2a2",
+    "url": "./static/js/runtime-main.958c92b4.js"
+  }
+]);
