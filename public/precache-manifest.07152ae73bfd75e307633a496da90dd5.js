452d095448272d7de1dc34f807f70490ed1399c4
jhonjaider1000 <jhonjaider100015@gmail.com>
2020-10-17 20:05:47 -0500

    ...

---
+++ public/precache-manifest.07152ae73bfd75e307633a496da90dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff641793a5cf34fd835d245142fdec1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebf250951f7837b53c6d",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "043834dd2232230b6fbc",
+    "url": "./static/js/2.950062e2.chunk.js"
+  },
+  {
+    "revision": "be76c1072259f9ba78917d9856358ff1",
+    "url": "./static/js/2.950062e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf250951f7837b53c6d",
+    "url": "./static/js/main.c09dea75.chunk.js"
+  },
+  {
+    "revision": "a7809c03c3c129a7d2a2",
+    "url": "./static/js/runtime-main.958c92b4.js"
+  }
+]);
